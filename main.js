d123e53000e8834f797127710d8d372919560620
Jonathan-C-git <yonathanculzoni@gmail.com>
2025-11-23 13:41:28 -0300

    Menu principal

---
+++ main.js
@@ -0,0 +1,8 @@
+let menu = "\t¡Bienvenidos!"+
+            "\nSeleccione una opción\n"+
+            "\n1- Lista de productos"+
+            "\n2- Comprar"+
+            "\n3- Ver carrito"+
+            "\n4- Salir";
+
+console.log(menu);
